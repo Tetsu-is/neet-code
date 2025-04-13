052c553e432b3a7ed6995ca0a71c4d9d6a885b6a
Tetsu-is <hap30305@kwansei.ac.jp>
2025-04-14 01:37:34 +0900

    solve valid-parentheses

--- go/main.go
+++ go/main.go
@@ -3,27 +3,32 @@ package main
 import (
 	"fmt"
 
+	"github.com/Tetsu-is/neet-code.git/stack"
 	"github.com/Tetsu-is/neet-code.git/two_pointers"
 )
 
 func main() {
-	in := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	in := "([])"
 	sol := Solution{}
-	out := sol.Trap(in)
+	out := sol.isValid(in)
 	fmt.Println("out: ", out)
 }
 
 type Solution struct {
 }
 
-func (s *Solution) ThreeSum(nums []int) [][]int {
+func (s *Solution) threeSum(nums []int) [][]int {
 	return two_pointers.ThreeSum(nums)
 }
 
-func (s *Solution) MaxArea(heights []int) int {
+func (s *Solution) maxArea(heights []int) int {
 	return two_pointers.MaxArea(heights)
 }
 
-func (s *Solution) Trap(height []int) int {
+func (s *Solution) trap(height []int) int {
 	return two_pointers.Trap(height)
+}
+
+func (sol *Solution) isValid(s string) bool {
+	return stack.IsValid(s)
 }
