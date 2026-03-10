0e6f723a47b918765309677fc3c75dbb6c74f052
Tetsu-is <tetsu.ismt@gmail.com>
2026-03-10 21:01:06 +0900

    solve

---
+++ trees/maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (root == NULL) {
+      return 0;
+    }
+    int ld = maxDepth(root->left);
+    int rd = maxDepth(root->right);
+    return max(ld, rd) + 1;
+  }
+};
+
+int main() {
+  Solution *s = new Solution();
+  // TreeNode *res = s->invertTree(&x1);
+
+  return 0;
+}
