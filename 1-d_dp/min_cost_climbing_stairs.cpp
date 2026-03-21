0baa48b01acf86e00cdeb941725f3fa35a6dc768
Tetsu-is <tetsu.ismt@gmail.com>
2026-03-21 23:24:29 +0900

    temp

--- 1-d_dp/min_cost_climbing_stairs.cpp
+++ 1-d_dp/min_cost_climbing_stairs.cpp
@@ -2,9 +2,26 @@
 using namespace std;
 
 class Solution {
+private:
+  vector<int> memo;
+
 public:
   int minCostClimbingStairs(vector<int> &cost) {
-    for
+    int n = cost.size();
+    memo.assign(n, -1);
+    int ret = 0 + min(dfs(0, cost), dfs(1, cost));
+    return ret;
+  }
+  int dfs(int i, vector<int> &cost) {
+    int len = (int)cost.size();
+    if (i >= len)
+      return 0;
+    if (memo[i] != -1) {
+      return memo[i];
+    }
+    int res = cost[i] + min(dfs(i + 1, cost), dfs(i + 2, cost));
+    memo[i] = res;
+    return res;
   }
 };
 
@@ -14,3 +31,6 @@ int main() {
   cout << s->minCostClimbingStairs(cost) << endl;
   return 0;
 }
+
+// dfs + memo: 2ms, 147.5MB
+//
